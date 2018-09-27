fa19631c65d5dd0fd7ed909405890db8261fef8c
zloy-zhake <jk-jdogg@yandex.ru>
2018-09-27 12:01:57 +0600

    Запустил apertium через sh

--- shell.py
+++ shell.py
@@ -1,8 +1,11 @@
 import sys
+import sh
 
 # прочитать текст со входа
 for line in sys.stdin:
 
-# сделать морфологический анализ и получить MorphLang_sl
-# сделать inference с помощью обученной модели
+    # сделать морфологический анализ и получить MorphLang_sl
+    print(sh.apertium(sh.echo(line), "-d", "/home/zhake/Source/apertium-eng-kaz", "eng-kaz"))
+
+# сделать inference с помощью обученной модели получить MorphLang_tl
 # сделать морфологическую генерацию
